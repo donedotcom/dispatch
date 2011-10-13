dd5c256ff345a41006e879273f0ea5893a684b4b
Josh Leitzel <joshleitzel@gmail.com>
2011-10-13 14:59:50 -0400

    Moved repetitive controller tests to a function; finished beforeHooks testing.

--- spec/controller_spec.js
+++ spec/controller_spec.js
@@ -25,7 +25,47 @@ var next = function () {
 
 // A global to inform whether a custom action has been called or not.
 // Set to false before defining a custom action (i.e., in the topic).
+var myCoolActionCalled = false;
 var customActionCalled = false;
+
+function testControllerActions (controller) {
+	var tests = {
+		'topic': controller,
+		'action processed correctly' : function (controller) {
+			var error = false;
+			try {
+				myCoolActionCalled = false;
+				req.action = 'myCoolAction';
+				controller.addRequest(req, res, next);
+			} catch (err) {
+				error = err;
+			}
+			assert.strictEqual(error, false);
+			assert.strictEqual(myCoolActionCalled, true);
+		},
+		'req has an implemented action' : function (controller) {
+			var error = false;
+			customActionCalled = false;
+			try {
+				req.action = 'myCoolAction';
+				controller.addRequest(req, res, next);
+			} catch (err) {
+				error = err;
+			}
+			assert.strictEqual(error, false);
+			assert.strictEqual(customActionCalled, true);
+		},
+		'req has an unimplemented action' : function (controller) {
+			try {
+				req.action = 'futureAction';
+				controller.addRequest(req, res, next);
+			} catch (err) {
+				assert.strictEqual(new RegExp(/has not been implemented yet/).test(err.message), true);
+			}
+		}
+	}
+	return tests;
+}
 
 vows.describe('Controller').addBatch({
   'controller initialization' : {
@@ -39,73 +79,34 @@ vows.describe('Controller').addBatch({
     }
   }
 }).addBatch({
-  'a controller with a beforeHook on one custom action' : {
-    topic : function () {
-			customActionCalled = false;
-			ForumsController = Controller.extend();
-			ForumsController.prototype.myCoolAction = function (req, res, next) {
-				customActionCalled = true;
-			}
-			ForumsController.before('myCoolAction', function (req, res, callback) {
-				callback();
-			});
-			return new ForumsController({
-				name: 'forums'
-			});
-		},
-    'req has an implemented action' : function (controller) {
-			var error = false;
-			try {
-				req.action = 'myCoolAction';
-				controller.addRequest(req, res, next);
-			} catch (err) {
-				error = err;
-			}
-			assert.strictEqual(error, false);
-			assert.strictEqual(customActionCalled, true);
-    },
-    'req has an unimplemented action' : function (controller) {
-			try {
-				req.action = 'futureAction';
-				controller.addRequest(req, res, next);
-			} catch (err) {
-				assert.strictEqual(new RegExp(/has not been implemented yet/).test(err.message), true);
-			}
-    }
-	}
+  'a controller with a beforeHook on one custom action' : testControllerActions(function () {
+		ForumsController = Controller.extend();
+		ForumsController.prototype.myCoolAction = function (req, res, next) {
+			myCoolActionCalled = true;
+			return;
+		};
+		ForumsController.before('myCoolAction', function (req, res, callback) {
+			customActionCalled = true;
+			callback();
+		});
+		return new ForumsController({
+			name: 'forums'
+		});
+	})
 }).addBatch({
-  'a controller with a beforeHook on all actions' : {
-    topic : function () {
-			customActionCalled = false;
-			ForumsController = Controller.extend();
-			ForumsController.prototype.myCoolAction = function (req, res, next) {
-				customActionCalled = true;
-			}
-			ForumsController.before('all', function (req, res, callback) {
-				callback();
-			});
-			return new ForumsController({
-				name: 'forums'
-			});
-		},
-    'req has an implemented action' : function (controller) {
-			var error = false;
-			try {
-				req.action = 'myCoolAction';
-				controller.addRequest(req, res, next);
-			} catch (err) {
-				error = err;
-			}
-			assert.strictEqual(error, false);
-			assert.strictEqual(customActionCalled, true);
-    },
-    'req has an unimplemented action' : function (controller) {
-			try {
-				req.action = 'futureAction';
-				controller.addRequest(req, res, next);
-			} catch (err) {
-				assert.strictEqual(new RegExp(/has not been implemented yet/).test(err.message), true);
-			}
-    }
-	}
+  'a controller with a beforeHook on all actions' : testControllerActions(function () {
+		customActionCalled = false;
+		ForumsController = Controller.extend();
+		ForumsController.prototype.myCoolAction = function (req, res, next) {
+			myCoolActionCalled = true;
+			return;
+		};
+		ForumsController.before('all', function (req, res, callback) {
+			customActionCalled = true;
+			callback();
+		});
+		return new ForumsController({
+			name: 'forums'
+		});
+	})
 }).export(module);
