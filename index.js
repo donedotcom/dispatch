b59bdf7ee263fd6ac178bbb9a4fd81b1af988466
Paul Covell <paul@done.com>
2011-11-17 16:07:50 -0500

    Added .config option to set options externally after loading (Issue #11)

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// This may be referenced by the other files, so it should be first.
+// You can configure the options externally
+exports.config = {
+  logger : {
+    console: {
+      level: 'warning'
+    }
+  }
+};
+
 exports.Controller = require('./lib/controller');
 exports.Router = require('./lib/router');
 exports.logger = require('./lib/logger');
